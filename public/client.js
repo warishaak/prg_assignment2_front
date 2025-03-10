9c60043842be5aaa0a72de1595893da4b2efa4a0
warishaak <167077087+warishaak@users.noreply.github.com>
2025-03-10 13:51:56 +0000

    Created photo memories page layout

--- public/client.js
+++ public/client.js
@@ -22,11 +22,15 @@ const getCoffeeDrinks = async () => {
     resultElement.textContent = `Error: ${error.message}`;
   }
 };
-
 document
     .getElementById("getCoffeeDrinks")
     .addEventListener("click", () => {
       // Navigate to coffeeDrinks.html
       window.location.href = 'coffeeDrinks.html';
     });
-
+document
+    .getElementById("getPhotos")
+    .addEventListener("click", () => {
+      // Navigate to coffeeDrinks.html
+      window.location.href = 'photoMemories.html';
+    });
