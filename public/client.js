fe0c6ab3091532e1f54385a5a9dc4a107359466d
warishaak <167077087+warishaak@users.noreply.github.com>
2025-03-10 14:32:36 +0000

    Refoctoring, removing unused code and comments

--- public/client.js
+++ public/client.js
@@ -1,27 +1,3 @@
-// Writing a function to communicate with our local server
-
-const getCoffeeDrinks = async () => {
-  const resultElement = document.getElementById("result");
-  resultElement.textContent = "Loading...";
-
-  try {
-    const response = await fetch(`/api/coffee_drinks`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    resultElement.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-  } catch (error) {
-    resultElement.textContent = `Error: ${error.message}`;
-  }
-};
 document
     .getElementById("getCoffeeDrinks")
     .addEventListener("click", () => {
